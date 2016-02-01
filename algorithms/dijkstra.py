45818078c39cee102d6a7a24f2d3f1b622d2420a
mlworle <mlworle@gmail.com>
2016-01-31 20:05:50 -0500

    du.py now has loop so if user makes a mistake they can correct it

---
+++ algorithms/dijkstra.py
@@ -0,0 +1,10 @@
+# Dijkstra's Shortest Path Algorithm
+# Inspired by Akamai Academy Networking Module and
+# Dr. Liz Borowsky's, Dir of Mappying,  Talk on 01-27-16
+
+# Read in Data
+# Determin Origin POint
+
+# Find all nodes connected to Origin by a single edege
+# Calculate distance to node
+# ch
--- du.py
+++ du.py
@@ -5,46 +5,65 @@
 
 # Getting lazy. One RTFM for all.
 def rtfm():
-  print "Bummer. Check your notes and try again. I don\'t mean to be mean, but there are a lot of commands and I have limited attention span to write thes, so I\'m not going to put this in another loop and keep checking if the answe3rs are right or if they were typed correctly or case matters, blahblahblah. So just start the program over. Thanks!"
+  print "Bummer. Check your notes and try again. I don\'t mean to be mean, but there are a lot of commands and I have limited attention span to write thes, so I\'m not going to put this in another loop and keep checking while the answe3rs are right or if they were typed correctly or case matters, blahblahblah. So just try again. Thanks!"
 
 
 # 1st Gate: understand meaning of command
-answer = raw_input("HI. Today we'll review the du command. First of all, can you tell me what du means?  ")
-if answer == "disk usage":
+def definition():
+  answer = raw_input("HI. Today we'll review the du command. First of all, can you tell me what du means?  ")
+  while answer != "disk usage":
+    rtfm()
   print "Bravo! You may move_on.\r\n\n"
-else: rtfm()
+
 
 # 2nd Gate: basic du command
-print "At the virtual command line prompt type the command to run disk usage. THis is the basic no-frills command. We'll get into more fancy stuff in a second.\r\n"
-ducommand = raw_input('$ ')
-if ducommand == "du" or "du ":
+def du():
+  print "At the virtual command line prompt type the command to run disk usage. THis is the basic no-frills command. We'll get into more fancy stuff in a second.\r\n"
+  ducommand = raw_input('$ ')
+  while ducommand != "du":
+    rtfm()
   print "Awesome! You Rock! Go on to the next command.\r"
-else: rtfm()
+
 
 # 3rd Gate: human readable form
-print "Now type in the command for running disk usage with more easily understood output. (Hint: read this if you really want to because you are a human not a computer.) \r\n"
-du_h_command = raw_input('$ ')
-if du_h_command == "du -h":
+def du_h():
+  print "Now type in the command for running disk usage with more easily understood output. (Hint: read this while you really want to because you are a human not a computer.) \r\n"
+  du_h_command = raw_input('$ ')
+  while du_h_command != "du -h":
+    rtfm()
   print "Awesome! You Rock and Roll! We are done. Go on to the next command, or not. \r\n"
-else: rtfm()
+
 
 # 4th Gate: -f files only
-print "Please type the command for disk usage for files."
-du_f_command = raw_input('$ ')
-if du_f_command == "du -f":
+def du_f():
+  print "Please type the command for disk usage for files."
+  du_f_command = raw_input('$ ')
+  while du_f_command != "du -f":
+    rtfm()
   print "Fantastic. Move on.\r\n"
-else: rtfm()
+
 
 # 5th Gate: -a all files and directories
-print "What would you type to see all the files and directories?\r"
-du_a_command = raw_input('$ ')
-if du_a_command == "du -a":
+def du_a():
+  print "What would you type to see all the files and directories?\r"
+  du_a_command = raw_input('$ ')
+  while du_a_command != "du -a":
+    rtfm()
   print "Great, keep going.\r\n"
-else: rtfm()
+
 
 # 6th Gate: -s all files and directories
-print "What would you type to see only the last line?\r"
-du_s_command = raw_input('$ ')
-if du_s_command == "du -s":
+def du_s():
+  print "What would you type to see only the last line?\r"
+  du_s_command = raw_input('$ ')
+  while du_s_command != "du -s":
+    rtfm()
   print "That\'s it. We\'re done. Check out another command.\r\n"
-else: rtfm()
+
+# Main
+definition()
+du()
+du_h()
+du_f()
+du_a()
+du_s()
